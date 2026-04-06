e08e2aadd8fd940fdf29d570ea8630b5e3a07a34
sakshamdias@examfriendly.in <sakshamdias@examfriendly.in>
2026-04-06 14:20:00 +0530

    newtab  Commit

--- navbar.js
+++ navbar.js
@@ -197,7 +197,7 @@ class ExamNavbar extends HTMLElement {
                     <a href="courses">Courses</a>
                     <a href="studentsResource">Student Resources</a>
                     <a href="aboutus">About Us</a>
-                    <a href="paperPilelauncher">Paper Pile Test Gen</a>
+                    <a href="/exam">Examination</a>
                     
                 </nav>
 
@@ -224,7 +224,7 @@ class ExamNavbar extends HTMLElement {
                 <a href="index">Home</a>
                 <a href="courses">Courses</a>
                 <a href="studentsResource">Resources</a>
-                <a href="paperPilelauncher">Test Gen</a>
+                <a href="/exam">Exam Centre</a>
                 <a href="aboutus">About Us</a>
             </nav>
         </div>
